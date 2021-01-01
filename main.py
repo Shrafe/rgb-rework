e9dd921e7def93419ce7841b73122fa0535a950a
Tom Hauser <tom.hauser@gmail.com>
2020-12-31 22:26:46 -0500

    fix verify.py; fully remove wx from relevant sections so it is not require to execute the program.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from math import hypot
 import random
 import sys
-import wx
 from PIL import Image
 import numpy as np
 
@@ -19,35 +18,14 @@ def load_target(path):
     data = np.array(target)
     print(f'image shape: {data.shape}')
     print(f'first element: {data[0]}')
-    # r, g, b = data[::3], data[1::3], data[2::3]
     return data
 
 def load_indexes():
     indexes = list(range(SIZE * SIZE))
     random.shuffle(indexes)
-    # indexes = sorted(indexes, key=index_func)
     return indexes
 
-def create_image_data(colors):
-    result = [None] * (SIZE * SIZE)
-    for index, (r, g, b) in enumerate(colors):
-        result[index] = chr(r) + chr(g) + chr(b)
-    return ''.join(result)
-
-def rgb_int(colors):
-    return [(r << 16) | (g << 8) | (b) for r, g, b in colors]
-
-def int_rgb(colors):
-    result = []
-    for color in colors:
-        r = 0xff & (color >> 16)
-        g = 0xff & (color >> 8)
-        b = 0xff & (color >> 0)
-        result.append((r, g, b))
-    return result
-
 def main(path):
-    app = wx.App()
     print('loading target image')
     target = load_target(path)
     reshapedTarget = target.reshape(4096*4096, 3)
@@ -64,7 +42,6 @@ def main(path):
         value = tree.pop(*reshapedTarget[index])
         colors[index] = [value[0], value[1], value[2]]
     print('creating output image')
-    # data = create_image_data(colors)
     image = Image.fromarray(np.array(colors.reshape(SIZE, SIZE, 3)))
     image.save('output.png')
 
@@ -72,10 +49,4 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python main.py input.png')
     else:
-        # array = np.zeros([SIZE*SIZE,3], dtype=np.uint8)
-        # print(array)
-        # print(array.reshape(SIZE*SIZE,3))
-        # print(array.flatten())
-        # img = Image.fromarray(array)
-        # img.save('testrgb.png') 
         main(sys.argv[1])
--- verify.py
+++ verify.py
@@ -1,17 +1,19 @@
 from itertools import product
 import sys
-import wx
+from PIL import Image
+import numpy as np
+
 
 def verify(path):
-    app = wx.App()
-    image = wx.Image(path)
-    data = image.GetData()
-    print(image)
-    data = [ord(x) for x in data]
-    if any(x < 0 or x > 255 for x in data):
+    image = Image.open(path)
+    data = np.array(image)
+    print(f'image shape: {data.shape}')
+    print(f'first element: {data[0]}')
+    if any(x < 0 or x > 255 for x in data.flatten()):
         raise Exception('Invalid: Values must be 0 <= x < 256.')
-    rgb = zip(data[::3], data[1::3], data[2::3])
-    if len(rgb) != len(set(rgb)):
+    rgb = data.reshape(4096*4096, 3)
+    mappable = map(tuple, rgb)
+    if len(rgb) != len(set(mappable)):
         raise Exception('Invalid: There are duplicate colors.')
     print('Valid!')
 
