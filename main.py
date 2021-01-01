2cfa1f213b7c7f6a74eb8c3924ac97e9d4db395d
Tom Hauser <tom.hauser@gmail.com>
2020-12-31 21:42:17 -0500

    working?

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from math import hypot
 import random
 import sys
 import wx
+from PIL import Image
+import numpy as np
 
 try:
     import octree_c as octree
@@ -13,39 +15,18 @@ except Exception:
 SIZE = 4096
 
 def load_target(path):
-    target = wx.Image(path)
-    data = target.GetData()
-    # data = [ord(x) for x in data]
-    r, g, b = data[::3], data[1::3], data[2::3]
-    return list(zip(r, g, b))
+    target = Image.open(path)
+    data = np.array(target)
+    print(f'image shape: {data.shape}')
+    print(f'first element: {data[0]}')
+    # r, g, b = data[::3], data[1::3], data[2::3]
+    return data
 
 def load_indexes():
-    points = [
-        (SIZE / 2, SIZE / 2),
-        # (2915, 1340),
-        # (1115, 1760),
-    ]
-    def index_func(index):
-        x, y = index % SIZE, index / SIZE
-        offset = (random.random() - 0.5) * 512
-        return min(hypot(x - a, y - b) for a, b in points) + offset
     indexes = list(range(SIZE * SIZE))
     random.shuffle(indexes)
     # indexes = sorted(indexes, key=index_func)
     return indexes
-
-def load_indexes_mask(path):
-    result = []
-    mask = load_target(path)
-    colors = sorted(set(mask))
-    groups = dict((x, []) for x in colors)
-    for index, color in enumerate(mask):
-        groups[color].append(index)
-    for color in colors:
-        group = groups[color]
-        random.shuffle(group)
-        result.extend(group)
-    return result
 
 def create_image_data(colors):
     result = [None] * (SIZE * SIZE)
@@ -69,26 +50,32 @@ def main(path):
     app = wx.App()
     print('loading target image')
     target = load_target(path)
+    reshapedTarget = target.reshape(4096*4096, 3)
     print('loading indexes')
     indexes = load_indexes()
-    # indexes = load_indexes_mask('mask.png')
     print('initializing octree')
     tree = octree.Octree()
     print('picking colors')
-    colors = [(0, 0, 0)] * (SIZE * SIZE)
+    colors = np.zeros([SIZE*SIZE,3], dtype=np.uint8)
     for i, index in enumerate(indexes):
         if i % 65536 == 0:
             pct = 100.0 * i / (SIZE * SIZE)
             print('{:.2f}'.format(pct)+' percent complete')
-        colors[index] = tree.pop(*target[index])
+        value = tree.pop(*reshapedTarget[index])
+        colors[index] = [value[0], value[1], value[2]]
     print('creating output image')
-    data = create_image_data(colors)
-    image = wx.Image(SIZE, SIZE)   
-    image.SetData(data.ToBytes())
-    image.SaveFile('output.png', wx.BITMAP_TYPE_PNG)
+    # data = create_image_data(colors)
+    image = Image.fromarray(np.array(colors.reshape(SIZE, SIZE, 3)))
+    image.save('output.png')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: python main.py input.png')
     else:
+        # array = np.zeros([SIZE*SIZE,3], dtype=np.uint8)
+        # print(array)
+        # print(array.reshape(SIZE*SIZE,3))
+        # print(array.flatten())
+        # img = Image.fromarray(array)
+        # img.save('testrgb.png') 
         main(sys.argv[1])
